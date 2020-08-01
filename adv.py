ba0bbfabfe0646be60bb21a10c6226f508524cc2
RobertPedersenDev <40696979+robpedersendev@users.noreply.github.com>
2020-07-31 22:40:25 -0600

    Added the code for the rest of the file. Renamed a few things. Tests are not passing.

--- adv.py
+++ adv.py
@@ -37,7 +37,7 @@ My code [and comments] will start here
 '''
 print("Houston, you read me? We are starting without you.")
 # I need a dictionary for my rooms
-graph = {}
+bobs_room_graph = {}
 # I need a to create a set for my visited rooms
 visited_rooms = set()
 # I need a dictionary that points to the opposite directions. This is used in conjunction with the reversed list.
@@ -48,7 +48,7 @@ reversed_directions = {
     "e": "w"
 }
 # And maybe someway to signal my truthy flag for a while loop that I visited all the rooms
-all_rooms_visited = False
+all_rooms_visited = True
 
 
 # I need two helper functions
@@ -60,28 +60,28 @@ all_rooms_visited = False
 # Take in the current rooms id, the directions you can go, the previous room ID number and direction, use KWARGS?
 def add_to_graph(room_id, directions, previous_room=None, previous_room_direction=None):
     # Using the rooms id as the index, instantiate a dictionary
-    graph[room_id] = {}
+    bobs_room_graph[room_id] = {}
     # Create a for loop that loops of the directions you can go
     for direction in directions:
         ## And if the direction you can go is the same as the previous rooms direction
         if direction == previous_room_direction:
             ### Use the dictionary created above to set the value of the key of the previous rooms ID to the previous rooms ID
-            graph[room_id][exit_option] = previous_room
+            bobs_room_graph[room_id][direction] = previous_room
         ## Otherwise
         else:
             ### Use the dictionary created above to set the value of the key of the previous rooms ID to a "?" per spec
-            graph[room_id][exit_option] = "?"
+            bobs_room_graph[room_id][direction] = "?"
 
 
 # Helper Function 2 - Getting the available moves inside a room
 # Take in all the directions you can go that you know of
-def get_directions(available_directions):
+def get_directions(players_available_directions):
     # Instantiate a list
     directions = []
     # Create a loop that loops over the directions you can go that you know of
-    for direction in available_directions:
+    for direction in players_available_directions:
         ## If the value of the key of the rooms index is "?"
-        if graph[room_id][direction] == "?":
+        if bobs_room_graph[players_current_room_id][direction] == "?":
             ### Add the known directions to the instantiated list above
             directions.append(direction)
     # return the list of directions
@@ -97,35 +97,63 @@ Now time for our main event
 add_to_graph(player.current_room.id, player.current_room.get_exits())
 
 # Create a while loop that runs until all rooms are visited
-## Using the imported player class, get the exits from the current room
-## Get the current room id from the player class
-## Maybe print out the data in a user friendly way?
+while all_rooms_visited:
+    ## Using the imported player class, get the exits from the current room
+    players_available_directions = player.current_room.get_exits()
+    ## Get the current room id from the player class
+    players_current_room_id = player.current_room.id
+    ## Maybe print out the data in a user friendly way?
+    print(f"Current room: {players_current_room_id} - {players_available_directions}")
+    print(bobs_room_graph)
+    print("~" * 10, "+" * 10,"~" * 10,)
 
-## Find the players available moves
-## Loop through the available directions to go
-## If the value of the key of the current rooms available direction is "?"
-## Add that option to the available moves list
-## If the length of the available moves list is 0
-## Then check if the length of the reversed list is 0
-## if it is, then set all rooms are visited value to True
-## Othersise
-## Reduce the reversed list by 1
-## Add to the Traversal Path the room that was removed from the reversed list above
-## Move the player using the player class
-
-## In case we get back to first room, reset return sequence by checking the
-## players current room against the worlds starting room
-## Otherwise
-## Grab the first available move and maybe print that out to be user friendly?
-## Add the direction your heading to the traversal path list
-## Add the exact opposite direction your heading in to the reversed list
-## Using the player class, move the player
-## Grab the players current room using the players class
-# add new_room_id to room_id's graph
-## Set the value of the the key of the index of my rooms graph to the value of the room id above
-## If the players room id does not exist in the players visited rooms set
-## Then we add the new room to the visited rooms list
-## And then add the players room id to the graph using the first helper function
+    ## Find the players available moves
+    available_moves = get_directions(players_available_directions)
+    ## Loop through the available directions to go
+    for available_move in players_available_directions:
+        ## If the value of the key of the current rooms available direction is "?"
+        if bobs_room_graph[players_current_room_id][available_move] == "?":
+            ## Add that option to the available moves list
+            available_moves.append(available_move)
+    ## If the length of the available moves list is 0
+    if len(available_moves) == 0:
+        ## Then check if the length of the reversed list is 0
+        if len(backwards_list) == 0:
+            ## if it is, then set all rooms are visited value to True
+            all_rooms_visited = True
+        ## Othersise
+        else:
+            ## Reduce the reversed list by 1
+            movement = backwards_list.pop(-1)
+            ## Add to the Traversal Path the room that was removed from the reversed list above
+            traversal_path.append(movement)
+            ## Move the player using the player class
+            player.travel(movement)
+            ## In case we get back to first room, reset return sequence by checking the
+            ## players current room against the worlds starting room
+            if player.current_room.id == world.starting_room:
+                backwards_list = []
+    ## Otherwise
+    else:
+        ## Grab the first available move and maybe print that out to be user friendly?
+        movement = available_moves[0]
+        print(f"moving {movement}")
+        ## Add the direction your heading to the traversal path list
+        traversal_path.append(movement)
+        ## Add the exact opposite direction your heading in to the reversed list
+        backwards_list.append(reversed_directions[movement])
+        ## Using the player class, move the player
+        player.travel(movement)
+        ## Grab the players current room using the players class
+        players_new_room_id = player.current_room.id
+        ## Set the value of the the key of the index of my rooms graph to the value of the room id above
+        bobs_room_graph[players_current_room_id][movement] = players_new_room_id
+        ## If the players room id does not exist in the players visited rooms set
+        if players_new_room_id not in visited_rooms:
+            ## Then we add the new room to the visited rooms list
+            visited_rooms.add(players_new_room_id)
+            ## And then add the players room id to the graph using the first helper function
+            add_to_graph(players_new_room_id, player.current_room.get_exits(), players_current_room_id, reversed_directions[movement])
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
